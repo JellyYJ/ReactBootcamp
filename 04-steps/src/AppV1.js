d2bea623708f03c2b334939f4c4f9c02271802e3
JellyYJ <lyj10000h@gmail.com>
2023-06-15 20:04:19 +0100

    update Steps project

--- 04-steps/src/AppV1.js
+++ 04-steps/src/AppV1.js
@@ -51,30 +51,22 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
               Previous
-              <span>🤑</span>
-            </Button>
-
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
               Next
-              <span>🤑</span>
-            </Button>
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
 
