3d4ea960ce728c3ebf0b5cf52255811a9329f81c
JellyYJ <lyj10000h@gmail.com>
2023-07-02 19:15:09 +0100

    Classy Weather-Class Components Exercise

---
+++ 09-classy-weather/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./AppV0";
+// import App from "./AppV1";
+import App from "./AppV2";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
