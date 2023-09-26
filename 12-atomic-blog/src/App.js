21cc4758e0d1af2ec8685c2705db16cc39799bd9
JellyYJ <lyj10000h@gmail.com>
2023-09-26 18:29:12 +0100

    add some comments

--- 12-atomic-blog/src/App.js
+++ 12-atomic-blog/src/App.js
@@ -135,12 +135,29 @@ function FormAddPost({ onAddPost }) {
       <textarea
         value={body}
         onChange={(e) => setBody(e.target.value)}
-        placeholder="Post body"
+        placeholder="Post content"
       />
       <button>Add post</button>
     </form>
   );
 }
+
+// Just for logging the "i to see
+// function List({ posts }) {
+//   return (
+//     <ul>
+//       {posts.map((post, i) => {
+//         console.log(i); // Log the 'i' value to the console
+//         return (
+//           <li key={i}>
+//             <h3>{post.title}</h3>
+//             <p>{post.body}</p>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// }
 
 function List({ posts }) {
   return (
@@ -156,7 +173,10 @@ function List({ posts }) {
 }
 
 function Archive({ onAddPost }) {
-  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
+  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState
+  // (which generates the posts) is only called once, on the initial render.
+  // So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render.
+  // We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
@@ -170,7 +190,6 @@ function Archive({ onAddPost }) {
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
-
       {showArchive && (
         <ul>
           {posts.map((post, i) => (
