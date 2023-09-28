1d5cf3cfa7df35b51762917cf15c6589b433e1a5
JellyYJ <lyj10000h@gmail.com>
2023-09-28 16:55:53 +0100

    use Context API

---
+++ 12-atomic-blog/README.md
@@ -0,0 +1,5 @@
+## Introduing Context API - useContext
+
+In the project directory, you can run:
+
+### `npm start`
--- 12-atomic-blog/src/index.js
+++ 12-atomic-blog/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import App from "./App";
+// import App from "./App";
+// import Appcontext from "./APPcontext";
+import APPcontextAdvanced from "./APPcontextAdvanced";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Appcontext /> */}
+    <APPcontextAdvanced />
   </React.StrictMode>
 );
