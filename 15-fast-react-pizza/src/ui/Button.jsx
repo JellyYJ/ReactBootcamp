d6706903c1d193f93d5091ad2ed6dff148baa7ef
JellyYJ <lyj10000h@gmail.com>
2023-11-20 14:16:49 +0000

    Styling the create order form

--- 15-fast-react-pizza/src/ui/Button.jsx
+++ 15-fast-react-pizza/src/ui/Button.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 // Define the Button component
 function Button({ children, disabled, to, type }) {
   const base =
-    'inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800  ring-offset-1 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 disabled:cursor-not-allowed';
+    'inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800  ring-offset-1 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 disabled:cursor-not-allowed';
 
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-3',
     small: base + ' px-3 py-2.5 md:px-4 md:py-2.5 text-xs',
     secondary:
-      ' px-4 py-3 md:px-6 md:py-2.5 inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-500  ring-offset-1 transition-colors duration-300 hover:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 disabled:cursor-not-allowed',
+      ' px-4 py-3 text-sm md:px-6 md:py-2.5 inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-500  ring-offset-1 transition-colors duration-300 hover:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 disabled:cursor-not-allowed',
   };
 
   // If it is a link
