5824162620565de83c157fbec95197c4a2f808bd
JellyYJ <lyj10000h@gmail.com>
2023-11-21 14:16:00 +0000

    Enable Geo location function

--- 15-fast-react-pizza/src/featrues/user/userSlice.js
+++ 15-fast-react-pizza/src/featrues/user/userSlice.js
@@ -1,29 +1,12 @@
-// function getPosition() {
-//   return new Promise(function (resolve, reject) {
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//   });
-// }
-
-// async function fetchAddress() {
-//   // 1) We get the user's geolocation position
-//   const positionObj = await getPosition();
-//   const position = {
-//     latitude: positionObj.coords.latitude,
-//     longitude: positionObj.coords.longitude,
-//   };
-
-//   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-//   const addressObj = await getAddress(position);
-//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
-
-//   // 3) Then we return an object with the data that we are interested in
-//   return { position, address };
-// }
-
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getAddress } from '../../services/apiGeocoding';
 
 const initialState = {
   username: '',
+  status: 'idle',
+  position: {},
+  address: '',
+  error: '',
 };
 
 const userSlice = createSlice({
@@ -34,7 +17,56 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+
+  // Implementing Geo location access
+  // Handle loading, fulfilled, and rejected states
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.status = 'idle';
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = 'error';
+        state.error = action.error.message;
+      }),
 });
+
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+/* 
+ Cannnot call async functions in redux, so we need THUNKS again
+ Thunks is a middleware sitting between the dispatch and ruducer
+ */
+export const fetchAddress = createAsyncThunk(
+  // action name
+  'user/fetchAddress',
+  // the actual thunk function
+  async function () {
+    // 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
+
+    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+    // 3) Then we return an object with the data that we are interested in
+    // payload of the fulfilled state which will be handled in the above extraReducer cases
+    return { position, address };
+  },
+);
 
 export const { updateName } = userSlice.actions;
 export default userSlice.reducer;
