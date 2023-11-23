e375fc78680bbe63de8b5977a1386112ab473093
JellyYJ <lyj10000h@gmail.com>
2023-11-23 12:46:12 +0000

    Project setup

---
+++ 16-the-wild-oasis/src/App.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: red;
+  padding: 20px;
+`;
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+
+        <Heading as="h2">The Wild Oasis</Heading>
+
+        <Button onClick={() => alert("Check in")}>Check In</Button>
+        <Button onClick={() => alert("Check Out")}>Check Out</Button>
+
+        <Heading as="h3">The Wild Oasis</Heading>
+        <Input type="number" placeholder="Number of guests"></Input>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
