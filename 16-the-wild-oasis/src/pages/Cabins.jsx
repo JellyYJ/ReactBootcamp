540d2b7b5df9a0e9aad548dbf4f4361ea19321dc
JellyYJ <lyj10000h@gmail.com>
2023-11-27 10:28:01 +0000

    using React Hook Form

--- 16-the-wild-oasis/src/pages/Cabins.jsx
+++ 16-the-wild-oasis/src/pages/Cabins.jsx
@@ -2,12 +2,17 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 import CabinTable from "../features/cabins/CabinTable";
+import CabinForm from "../features/cabins/CreateCabinForm";
+import Button from "../ui/Button";
+import { useState } from "react";
 
 function Cabins() {
   // Testing if we can fetch data from Supabase
   //   useEffect(function () {
   //     getCabins().then((data) => console.log(data));
   //   }, []);
+
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <>
@@ -18,6 +23,10 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add New Cabin
+        </Button>
+        {showForm && <CabinForm />}
       </Row>
     </>
   );
