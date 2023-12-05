0801439e1058c8bceb6dfab4041f904fafb4b95f
JellyYJ <lyj10000h@gmail.com>
2023-12-05 17:55:22 +0000

    Sort bookings

--- 16-the-wild-oasis/src/features/bookings/useBookings.js
+++ 16-the-wild-oasis/src/features/bookings/useBookings.js
@@ -3,23 +3,28 @@ import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
-  // API-side filtering
+  // API-SIDE filter and sort
   const [searchParams] = useSearchParams();
-
+  // Filter
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
-  // { field: "regularPrice", value: 5000, method: "gte" };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
+  // Sort
+  const sortByRow = searchParams.get("sortBy") || "startDate-desc"; // default value: startDate-desc
+  const [field, direction] = sortByRow.split("-");
+  const sortBy = { field, direction };
 
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
--- 16-the-wild-oasis/src/services/apiBookings.js
+++ 16-the-wild-oasis/src/services/apiBookings.js
@@ -15,6 +15,14 @@ export async function getBookings({ filter, sortBy }) {
     // More flexible way
     query = query[filter.method || "eq"](filter.field, filter.value);
   }
+
+  // Sort
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+  }
+
   const { data, error } = await query;
 
   if (error) {
